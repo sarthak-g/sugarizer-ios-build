27b84e53225c06e1ae023592f21d36dda1bb4d01
Sarthak <sarthak@Sarthaks-Mac.local>
2021-06-12 22:55:28 -0700

    make_ios.sh and exclude.ios

---
+++ make_ios.sh
@@ -0,0 +1,42 @@
+date
+
+echo --- Deleting previous content...
+cordova plugin remove cordova-plugin-sugarizeros
+#cp platforms/ios/Sugarizer/Sugarizer-Info.plist .
+rm config.xml
+cd www
+rm -rf *
+cd ..
+
+
+echo --- Minimize
+if [ "$1" != "full" -a "$2" != "full" ]; then
+  cd ../sugarizer
+  grunt -v
+  cd ../Sugarizer_cordova
+fi
+
+echo --- Copying content
+rsync -av --exclude-from='exclude.ios' ../sugarizer/* www
+cp ../sugarizer/config.xml .
+sed -i -e "s/\({\"id\": \"org.sugarlabs.TurtleBlocksJS\",.*\},\)//" www/activities.json
+sed -i -e "s/\({\"id\": \"org.somosazucar.JappyActivity\",.*\},\)//" www/activities.json
+#sed -i -e 's/class="offlinemode"//' www/activities/Scratch.activity/index.html
+rm -rf www/activities/Jappy.activity www/activities/TurtleBlocksJS.activity
+
+Cordova plugin add https://github.com/AraHovakimyan/cordova-plugin-wkwebviewxhrfix
+
+echo --- Build Cordova debug version
+cordova -d build ios 
+cp ../Sugarizer/res/icon/ios/* res/icon/ios
+cp ../Sugarizer/res/icon/ios/* platforms/ios/Sugarizer/Images.xcassets/AppIcon.appiconset
+#cp ../Sugarizer/res/splash/ios/* platforms/ios/Sugarizer/Images.xcassets/LaunchImage.launchimage
+#cp Sugarizer-Info.plist platforms/ios/Sugarizer
+
+echo --- Use XCode to generate and copy to device
+
+date
+
+
+
+
